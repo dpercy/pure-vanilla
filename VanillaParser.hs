c9a69a81b37d13c47cdc94986286d92e64437a6a
David Percy <davetp425@gmail.com>
2016-12-15 03:20:24 -0500

    pass more tests

--- VanillaParser.hs
+++ VanillaParser.hs
@@ -30,55 +30,114 @@ let iden = expr0 in expr1  -- sugar for ((iden) -> expr1)(expr0)
 
 type Parser = ParsecT [Char] () Identity
 
+{-
+TODO distinguish ast parsers from token parsers:
+don't have to use Parsec's GenTokenParser thing,
+but instead just define tok_id etc.
+-}
+
 token :: Parser a -> Parser a
 token p = do
   v <- p
   spaces
   return v
 
+tok_equals = token $ char '='
+tok_id = token $ many1 letter -- TODO digit, underscore...
+tok_op = token $ many1 (oneOf "~!@#$%^&*-=+|\\:<>/?")
+tok_semicolon = token $ char ';'
+tok_openParen = token $ char '('
+tok_closeParen = token $ char ')'
+tok_comma = token $ char ','
+tok_arrow = token $ string "->"
+parens = between tok_openParen tok_closeParen
+
 program :: Parser [Def]
 program = do
   spaces
-  sepBy def (token $ char ';')
+  sepBy def tok_semicolon
 
 def :: Parser Def
 def = do
-  lhs <- iden
-  token $ char '='
+  lhs <- tok_id
+  tok_equals
   rhs <- expr
   return $ Def lhs rhs
 
-iden :: Parser String
-iden = token $ do
-  hd <- letter
-  tl <- many (letter <|> digit)
-  return $ hd:tl
-
 expr :: Parser Expr
-expr = (Var `liftM` iden)
-       <|> literal
+expr = do
+  primary <- (literal
+              <|> Var `liftM` tok_id
+              <|> lambda
+              <|> try (Var `liftM` parens tok_op)
+              <|> prefixOp
+              <|> parens expr)
+  call primary <|> infixOp primary <|> return primary
 
 literal :: Parser Expr
 literal = token $ (Lit . Integer . read) `liftM` many1 digit
 
+lambda :: Parser Expr
+lambda = do p <- try $ do p <- params
+                          tok_arrow
+                          return p
+            e <- expr
+            return $ Func p e
+    where params = parens (param `sepBy` tok_comma)
+          param = tok_id <|> tok_op
 
+call :: Expr -> Parser Expr
+call callee = do args <- parens (expr `sepBy` tok_comma)
+                 return $ App callee (foldr Cons (Lit Null) args)
+
+prefixOp :: Parser Expr
+prefixOp = do op <- tok_op
+              arg <- expr
+              return $ App (Var op) [arg]
+
+infixOp arg0 = do op <- tok_op
+                  arg1 <- expr
+                  return $ App (Var op) [arg0, arg1]
+
+
+pp s = case parse program "<in>" s of
+  Left err -> error $ show err
+  Right v -> v
 
 prop_empty =
-  parse program "<in>" "" == Right []
+  pp "" == []
   
 prop_example =
-  parse program "<in>" " x = 1 ; y = 2 "
-   == Right [ Def "x" (Lit $ Integer 1)
-            , Def "y" (Lit $ Integer 2)
-            ]
+  pp " x = 1 ; y = 2 "
+   == [ Def "x" (Lit $ Integer 1)
+      , Def "y" (Lit $ Integer 2)
+      ]
+
+prop_func =
+  pp "f = () -> 1"
+  == [ Def "f" (Func [] (Lit $ Integer 1)) ]
+
+prop_params =
+  pp "f = (x, y, ++) -> 1"
+  == [ Def "f" (Func ["x", "y", "++"] (Lit $ Integer 1)) ]
+
+prop_call =
+  pp "v = f(x, 1)"
+  == [ Def "v" (App (Var "f") [(Var "x"), (Lit $ Integer 1)]) ]
+
+prop_prefix =
+  pp "f = () -> (- x)"
+  == [  Def "f" (Func []
+                 (App (Var "-") [Var "x"]))
+     ]
 
 prop_ops =
-  parse program "<in>" "f = (++, <|>) -> (<|> (1 ++ 2))"
-  == Right [ Def "f" (Func ["++", "<|>"]
-                      (App (Var "<|>")
-                       [(App (Var "++")
-                         [(Lit $ Integer 1), (Lit $ Integer 2)])]))
-           ]
+  pp "f = (++, <|>) -> (<|> (1 ++ 2))"
+  == [ Def "f" (Func ["++", "<|>"]
+                (App (Var "<|>")
+                 [(App (Var "++")
+                   [(Lit $ Integer 1), (Lit $ Integer 2)])]))
+     ]
 
 
 -- scary quickCheck macros!
