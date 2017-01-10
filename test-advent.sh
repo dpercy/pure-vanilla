f05de6955fa52d2a740d349c4b9b5e8ac354885f
David Percy <davetp425@gmail.com>
2017-01-10 00:45:05 -0500

    time tests

--- test-advent.sh
+++ test-advent.sh
@@ -5,6 +5,6 @@ dir=examples/advent2016
 for i in $(ls $dir | grep '^[0-9]*$' | sort -n)
 do
     diff \
-        <(set -x ; stack exec pure-vanilla interact $dir/$i <$dir/$i.input) \
+        <(set -x ; time stack exec pure-vanilla interact $dir/$i <$dir/$i.input) \
         $dir/$i.output
 done
