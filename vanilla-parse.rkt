be63b13dca8465962af559203ddcf8feaff54f79
David Percy <davetp425@gmail.com>
2017-02-15 22:48:35 -0500

    Function arrows

--- vanilla-parse.rkt
+++ vanilla-parse.rkt
@@ -47,7 +47,7 @@
 
 
 
-(struct Program (defs) #:transparent)
+(struct Program (statements) #:transparent)
 (struct Def (var expr) #:transparent)
 
 (struct Lit (value) #:transparent)
@@ -55,6 +55,7 @@
 (struct Global (mod name) #:transparent)
 
 (struct Func (params body) #:transparent)
+(struct Call (func args) #:transparent)
 
 (define parse
   (parser
@@ -63,24 +64,85 @@
    (end EOF)
    (error (lambda (tok-ok? tok-name tok-value)
             (error 'parse "Unexpected token ~v ~v" tok-name tok-value)))
+
+   (precs (nonassoc Arrow)
+          (nonassoc OpenParen)
+          )
    (grammar
-    (Program [() (list)]
-             [(DefOrExpr) (list $1)]
-             [(DefOrExpr Newline Program) (cons $1 $3)])
-    (DefOrExpr [(Def) $1] [(Expr) $1])
+    (Program [(Statements) (Program $1)])
+    (Statements [() (list)]
+                [(Statement) (list $1)]
+                [(Statement Newline Statements) (cons $1 $3)])
+    (Statement [(Def) $1] [(Expr) $1])
     (Def [(Iden Equals Expr) (Def $1 $3)])
     (Iden [(UnqualIden) (Global #f $1)] ; TODO pass in the default namespace and current scope
           [(IdenNum) (match $1 [(list name num) (Local name num)])]
           [(QualIden) (match $1 [(list mod name) (Global mod name)])])
-    (Expr [(Num) (Lit $1)]
-          [(Iden) $1]
-          [(OpenParen Params CloseParen Arrow Expr) (Func $2 $5)]
-          )
-    (Params [() (list)] ; empty case, or base case with trailing comma
-            [(Iden) (list $1)] ; base case with no trailing comma
-            [(Iden Comma Params) (cons $1 $3)]))))
+    (Expr [(ExprExceptIden) $1]
+          [(Iden) $1])
+    (ExprExceptIden [(Num) (Lit $1)]
+                    [(Expr OpenParen Args CloseParen) (Call $1 $3)]
+
+                    [(OpenParen ExprExceptIden CloseParen) $2]
+                    [(OpenParen Iden CloseParen) $2]
+
+                    ; several cases for function arrows
+                    ; x -> 1
+                    [(Iden Arrow Expr) (Func (list $1) $3)]
+                    ; (x) -> 1
+                    [(OpenParen Iden CloseParen Arrow Expr) (Func (list $2) $5)]
+                    ; (x,) -> 1
+                    [(OpenParen Iden Comma CloseParen Arrow Expr) (Func (list $2) $6)]
+                    ; () -> 1
+                    [(OpenParen CloseParen Arrow Expr) (Func '() $4)]
+                    ; (x, y, ...) -> 1
+                    [(OpenParen TwoOrMoreParams CloseParen Arrow Expr) (Func $2 $5)])
+    (NonemptyParams [(Iden) (list $1)] ; base case with no trailing comma
+                    [(Iden Comma) (list $1)] ; base case with trailing comma
+                    [(Iden Comma NonemptyParams) (cons $1 $3)])
+    (TwoOrMoreParams [(Iden Comma NonemptyParams) (cons $1 $3)])
+    (Params [() (list)] ; empty case
+            [(Iden) (list $1)]
+            [(Iden Comma NonemptyParams) (cons $1 $3)] ; two more more case
+            )
+    (Args [() (list)] ; empty case, or base case with trailing comma
+          [(Expr) (list $1)] ; base case with no trailing comma
+          [(Expr Comma Args) (cons $1 $3)]))))
 
 (define (parse-string str)
   (let* ([port (open-input-string str)]
          [lex! (lambda () (lex port))])
     (parse lex!)))
+(module+ test
+  (require rackunit)
+
+  (check-equal? (parse-string "f(x, 1)")
+                (Program (list (Call (Global #f 'f) (list (Global #f 'x)
+                                                          (Lit 1))))))
+  (check-equal? (parse-string "f = () -> 4")
+                (Program (list (Def (Global #f 'f) (Func '() (Lit 4))))))
+
+  ; application binds tighter than abstraction
+  (check-equal? (parse-string "() -> 1")
+                (Program (list (Func '() (Lit 1)))))
+
+  ; parenthesized expr vs function
+  (check-equal? (parse-string "(x)") (Program (list (Global #f 'x))))
+  (check-equal? (parse-string "(x) -> 3") (Program (list (Func (list (Global #f 'x))
+                                                               (Lit 3)))))
+
+  ; function arity
+  (check-equal? (parse-string "(x,) -> 3") (Program (list (Func (list (Global #f 'x))
+                                                                (Lit 3)))))
+  (check-equal? (parse-string "(x,y) -> 3") (Program (list (Func (list (Global #f 'x)
+                                                                       (Global #f 'y))
+                                                                 (Lit 3)))))
+  (check-equal? (parse-string "(x,y,) -> 3") (Program (list (Func (list (Global #f 'x)
+                                                                        (Global #f 'y))
+                                                                  (Lit 3)))))
+
+  ; expr nesting inside parens
+  (check-equal? (parse-string "((x))") (Program (list (Global #f 'x))))
+  (check-equal? (parse-string "(((x)))") (Program (list (Global #f 'x))))
+  (check-equal? (parse-string "(x -> 1)") (Program (list (Func (list (Global #f 'x))
+                                                               (Lit 1))))))
