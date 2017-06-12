58ed072930d05007e09f4114199a374a7d47886d
David Percy <davetp425@gmail.com>
2017-06-11 22:32:22 -0400

    Update constructor

--- constructor.rkt
+++ constructor.rkt
@@ -1,6 +1,15 @@
 #lang racket
 
-(provide (all-defined-out))
+(provide construct
+         destruct
+         make-symbol
+         typetag
+         tagof
+
+         Tagged?
+         Typetag?
+
+         tag-struct)
 
 (require racket/splicing
          racket/syntax
@@ -8,20 +17,30 @@
 
 
 (struct Tagged (tag values) #:transparent)
+(struct Typetag (tag) #:transparent)
 
 
 (define/contract (make-symbol mod name) (-> string? string? symbol?)
   (format-symbol "~a.~a" mod name))
 
 
-(define/contract (tag t args) (-> symbol? list? Tagged?)
+(define/contract (construct t args) (-> symbol? list? Tagged?)
   (Tagged t args))
 
 
-(define/contract (untag t v) (-> symbol? any/c (or/c #false list?))
+(define/contract (destruct t v) (-> symbol? any/c (or/c #false list?))
   (match v
     [(Tagged (== t) args) args]
     [(Tagged _ _) #false]))
+
+
+(define/contract (typetag t) (-> symbol? Typetag?)
+  (Typetag t))
+
+
+(define/contract (tagof v) (-> Tagged? Typetag?)
+  (match v
+    [(Tagged t _) (typetag t)]))
 
 
 (define-syntax (tag-struct stx)
@@ -40,19 +59,33 @@
              (define-match-expander name
                ; pat
                (syntax-rules ()
-                 [(_ args ...)  (app (curry untag t) (list args ...))])
+                 [(_ args ...)  (app (curry destruct t) (list args ...))])
                ; expr
                (make-set!-transformer
                 (lambda (stx)
                   (if (identifier? stx)
                       #'(lambda (args ...) (name args ...))
                       ((syntax-rules ()
-                         [(_ args ...)  (tag t (list args ...))])
+                         [(_ args ...)  (construct t (list args ...))])
                        stx)))))
              (define (predicate v)
-               (and (Tagged? v) (equal? (Tagged-tag v) t)))
+               (and (Tagged? v) (equal? (tagof v) (typetag t))))
              (define/contract (accessor v)
                (-> predicate any/c)
-               (list-ref (untag t v) index)) ...
+               (list-ref (destruct t v) index)) ...
              ;;
              )))]))
+(module+ test
+  (require rackunit)
+
+  (tag-struct "Foo" (Bar x y))
+
+  (check-equal? (format "~v" (Bar 1 2))
+                "(Tagged 'Foo.Bar '(1 2))")
+  (check-pred Bar? (Bar 1 2))
+  (check-equal? (tagof (Bar 1 2)) (typetag 'Foo.Bar))
+
+  (match (Bar 1 2)
+    [(Bar x y) (begin
+                 (check-equal? x 1)
+                 (check-equal? y 2))]))
